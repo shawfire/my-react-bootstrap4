c33aaa374580c051edbde2158d19e6bc2292389e
John Shaw <shaw_fire@yahoo.com>
2017-05-30 11:31:58 +1000

    React stateless funtional component - static html.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Alert } from './components/Alert.js'
+
+class App extends Component {
+  render() {
+    return (
+      <Alert />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Alert.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Alert = () => (
+  <div className="alert alert-success" role="alert">
+    <strong>Well done!</strong> You successfully read this important alert message.
+  </div>
+)
